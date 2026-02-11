4da6538ac310c32641cd307b24ca8002b33d3798
Shreya <shreyavnadiger@gmail.com>
2026-02-11 10:01:44 +0530

    chat

---
+++ my-project/src/app.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import './app.css'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+
+function App() {
+  return (
+    <Router>
+      <nav style={{ padding: '1rem', backgroundColor: '#333', marginBottom: '1rem' }}>
+        <Link to="/" style={{ color: 'white', marginRight: '1rem' }}>Home</Link>
+        <Link to="/about" style={{ color: 'white', marginRight: '1rem' }}>About</Link>
+        <Link to="/contact" style={{ color: 'white' }}>Contact</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
